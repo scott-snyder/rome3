b4db456f25dcb3be5b1f3cc78cc5a5ce234a7c48
Ryu Sawada <sawada@icepp.s.u-tokyo.ac.jp>
2015-02-20 01:54:06 +0100

    Change release flag

--- tools/UpdateVersionH/main.cpp
+++ tools/UpdateVersionH/main.cpp
@@ -26,7 +26,7 @@ using namespace std;
 const Int_t romeMajor       =  3; // format must be changed when it is larger than 9
 const Int_t romeMinor       =  1; // must be less than 100
 const Int_t romePatch       =  6; // must be less than 100
-const Int_t isStableVersion =  1; // 0: development version, 1: stable version
+const Int_t isStableVersion =  0; // 0: development version, 1: stable version
 //------
 
 /*
