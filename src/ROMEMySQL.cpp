e9e0b26f1039f6d16db9e9823aa0b6cfad4f3daf
Yusuke Uchiyama <uchiyama@icepp.s.u-tokyo.ac.jp>
2022-06-14 16:19:51 +0200

    Avoid possible memory leak.
    Delete 'result' before use.

--- src/ROMEMySQL.cpp
+++ src/ROMEMySQL.cpp
@@ -16,10 +16,10 @@ ClassImp(ROMEMySQL)
 
 //______________________________________________________________________________
 ROMEMySQL::ROMEMySQL()
-:ROMESQL()
-,mysql()
-,result(0)
-,row()
+   : ROMESQL()
+   , mysql()
+   , result(0)
+   , row()
 {
    mysql_init(&mysql);
 }
@@ -31,9 +31,9 @@ ROMEMySQL::~ROMEMySQL()
 }
 
 //______________________________________________________________________________
-Bool_t ROMEMySQL::Connect(const char *server,const char *user,const char *passwd,const char *database,const char *port)
+Bool_t ROMEMySQL::Connect(const char *server, const char *user, const char *passwd, const char *database, const char *port)
 {
-   if (!mysql_real_connect(&mysql, server, user, passwd, database,atoi(port), NULL, 0)) {
+   if (!mysql_real_connect(&mysql, server, user, passwd, database, atoi(port), NULL, 0)) {
       ROMEPrint::Error("Could not connect to the data base '%s' : Error: %s\n", database, GetErrorMessage());
       return false;
    }
@@ -48,6 +48,7 @@ Bool_t ROMEMySQL::Connect(const char *server,const char *user,const char *passwd
 Bool_t ROMEMySQL::DisConnect()
 {
    mysql_close(&mysql);
+   FreeResult();
    return true;
 }
 
@@ -64,9 +65,10 @@ Bool_t ROMEMySQL::MakeQuery(const char* query, Bool_t store)
       ROMEPrint::Warning("SQL connection is lost. Program sleeps for 5s and tries again.\n");
       gSystem->Sleep(5000);
    }
-   if (strlen(query) < 2048)
+   if (strlen(query) < 2048) {
       ROMEPrint::Debug("ROMEMySQL::MakeQuery : %s\n", query);
-   if (mysql_query(&mysql,query)) {
+   }
+   if (mysql_query(&mysql, query)) {
       ROMEPrint::Error("%s\n", query);
       ROMEPrint::Error("Query error : %s\n", GetErrorMessage());
       return false;
@@ -82,8 +84,13 @@ Bool_t ROMEMySQL::MakeQuery(const char* query, Bool_t store)
 //______________________________________________________________________________
 Bool_t ROMEMySQL::StoreResult()
 {
-   if (!(result = mysql_store_result(&mysql)))
+   if (result) {
+      FreeResult();
+   }
+
+   if (!(result = mysql_store_result(&mysql))) {
       return false;
+   }
    return true;
 }
 
@@ -127,7 +134,7 @@ Bool_t ROMEMySQL::DataSeek(my_ulonglong offset)
       ROMEPrint::Error("DataSeek error : offset is larger than number of results");
       return false;
    }
-   mysql_data_seek(result,offset);
+   mysql_data_seek(result, offset);
    if (!(row = mysql_fetch_row(result))) {
       ROMEPrint::Error("DataSeek error : %s\n", GetErrorMessage());
       return false;
@@ -181,7 +188,7 @@ Bool_t ROMEMySQL::StartTransaction(const char* option)
    TString sqlQuery = "START TRANSACTION ";
    sqlQuery += option;
    sqlQuery += ";";
-   return MakeQuery(sqlQuery.Data(),false);
+   return MakeQuery(sqlQuery.Data(), false);
 
 }
 
@@ -191,7 +198,7 @@ Bool_t ROMEMySQL::CommitTransaction(const char* option)
    TString sqlQuery = "COMMIT ";
    sqlQuery += option;
    sqlQuery += ";";
-   return MakeQuery(sqlQuery.Data(),false);
+   return MakeQuery(sqlQuery.Data(), false);
 }
 
 //______________________________________________________________________________
@@ -200,5 +207,5 @@ Bool_t ROMEMySQL::RollbackTransaction(const char* option)
    TString sqlQuery = "ROLLBACK ";
    sqlQuery += option;
    sqlQuery += ";";
-   return MakeQuery(sqlQuery.Data(),false);
+   return MakeQuery(sqlQuery.Data(), false);
 }
